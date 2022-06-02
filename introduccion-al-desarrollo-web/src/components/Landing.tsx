2ab263e2a5389fb44bc7523430cf5aee41ed011d
felipendelicia <delicia4581@gmail.com>
2022-06-02 16:19:18 -0300

    feat: landing.tsx with the styles

--- introduccion-al-desarrollo-web/src/components/Landing.tsx
+++ introduccion-al-desarrollo-web/src/components/Landing.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import "../styles/Landing.css"
+import "../styles/Landing.css";
 
 const Landing = () => {
   return (
     <div className="landing-container-main">
-        <div className="landing-container">
-            
-        </div>
+      <div className="landing-container">
+        <h1>Introducite al desarrollo web! 👨‍💻</h1>
+        <p>
+          Hola! soy Felipe y mi objetivo es crear una serie de posibles pasos a seguir para
+          poder introducir a alguien al desarrollo web mediante una serie de
+          contenidos que te permitan ser capaz de desempeñarse de forma
+          autodidacta.
+        </p>
+        <button> Arranquemos! </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
