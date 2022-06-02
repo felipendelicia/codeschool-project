9c6ce44099ca3d7f450fc76977695fea3b20eec6
felipendelicia <delicia4581@gmail.com>
2022-06-02 17:24:27 -0300

    feat: adding step view

--- introduccion-al-desarrollo-web/src/views/Landing.tsx
+++ introduccion-al-desarrollo-web/src/views/Landing.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import BigTitle from "../components/BigTitle";
 import Paragraph from "../components/Paragraph";
@@ -11,12 +12,14 @@ const Landing = () => {
         <div className="landing-container">
           <BigTitle>Aprendé desarrollo web 👩‍💻</BigTitle>
           <Paragraph>
-            Hola! soy Felipe y mi objetivo es crear una serie de posibles pasos a
-            seguir para poder introducir a alguien al desarrollo web mediante una
-            serie de contenidos que te permitan ser capaz de desempeñarse de forma
-            autodidacta.
+            Hola! soy Felipe y mi objetivo es crear una serie de posibles pasos
+            a seguir para poder introducir a alguien al desarrollo web mediante
+            una serie de contenidos que te permitan ser capaz de desempeñarse de
+            forma autodidacta.
           </Paragraph>
-          <Button> Arranquemos! </Button>
+          <Link to="/introduccion">
+            <Button> Arranquemos! </Button>
+          </Link>
         </div>
       </div>
     </ViewContainer>
